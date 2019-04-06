ae0e533a88b81120ee3a6699ac142c1143b3c3c4
QuantumlyTangled <EthoArx@gmail.com>
2019-04-06 15:18:50 +0200

    dis is a test

--- build.js
+++ build.js
@@ -1,19 +1,26 @@
 const { emptyDir } = require('fs-nextra');
 const { readFileSync, writeFileSync } = require('fs');
+const { promisify } = require('util');
+const { exec } = require('child_process');
 const { resolve } = require('path');
 const rimraf = require('rimraf');
 
-emptyDir(`${__dirname}/dist`);
+const pExec = promisify(exec);
+
+rimraf.sync(`${__dirname}/dist`);
 
 const TSCONFIG_FILE = resolve(__dirname, 'tsconfig.json');
+const PACKAGE_FILE = resolve(__dirname, 'package.json');
 const KLASA_DIR = resolve(__dirname, 'node_modules', 'klasa');
 const DJS_DIR = resolve(__dirname, 'node_modules', 'discord.js');
 
 const CONFIG_START = '		';
+const PACKAGE_START = '    ';
 
-let tscContent = readFileSync(TSCONFIG_FILE, { encoding: 'utf8' });
+const ORIGINAL_TSC_CONTENT = readFileSync(TSCONFIG_FILE, { encoding: 'utf8' });
+const ORIGINAL_PACKAGE_CONTENT = readFileSync(PACKAGE_FILE, { encoding: 'utf8' });
 
-tscContent = tscContent
+const tscContent = ORIGINAL_TSC_CONTENT
   .replace(`${CONFIG_START}"noEmitOnError": true`, `${CONFIG_START}"noEmitOnError": false`)
   .replace(`${CONFIG_START}"noImplicitAny": true`, `${CONFIG_START}"noImplicitAny": false`)
   .replace(`${CONFIG_START}"noImplicitReturns": true`, `${CONFIG_START}"noImplicitReturns": false`)
@@ -22,7 +29,14 @@ tscContent = tscContent
   .replace(`${CONFIG_START}"noUnusedParameters": true`, `${CONFIG_START}"noUnusedParameters": false`)
   .replace(`${CONFIG_START}"strictBindCallApply": true,\n`, '');
 
+const packageContent = ORIGINAL_PACKAGE_CONTENT
+  .replace(`${PACKAGE_START}"klasa": "github:dirigeants/klasa#master",\n`, '');
+
 writeFileSync(TSCONFIG_FILE, tscContent);
+writeFileSync(PACKAGE_FILE, packageContent);
 
 rimraf.sync(KLASA_DIR);
 rimraf.sync(DJS_DIR);
+
+pExec(`tsc --project "${resolve(__dirname)}"`)
+  .then(() => writeFileSync(TSCONFIG_FILE, original_tscContent));
