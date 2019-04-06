e8ef2c596abd59b25f75d882d81133d61cb43741
QuantumlyTangled <EthoArx@gmail.com>
2019-04-06 17:55:33 +0200

    oof

--- build.js
+++ build.js
@@ -3,7 +3,6 @@ const { readFileSync, writeFileSync } = require('fs');
 const { promisify } = require('util');
 const { exec } = require('child_process');
 const { resolve } = require('path');
-const rimraf = require('rimraf');
 
 const pExec = promisify(exec);
 
